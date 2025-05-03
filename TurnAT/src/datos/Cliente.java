e13f969bb8a8bd77e0ff2647fc73892042371cb5
Santiago Serrano <santiislas@outlook.es>
2025-05-03 17:52:21 -0300

    complete estas clases

--- TurnAT/src/datos/Cliente.java
+++ TurnAT/src/datos/Cliente.java
@@ -1,5 +1,35 @@
 package datos;
 
-public class Cliente {
+public class Cliente extends Persona{
+	
+	private String email;
+	private int telefono;
+	
+	public Cliente() {}
+	public Cliente(String nombre, String apellido, int dni, String email, int telefono) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+		this.email = email;
+		this.telefono = telefono;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	@Override
+	public String toString() {
+		return "Cliente [email=" + email + ", telefono=" + telefono + "]";
+	}
+	
 
 }
