2902570e1035cab3e39204f5fa2148e3d6569836
ltisera <lucastisera@gmail.com>
2025-05-04 21:51:29 -0300

    algunas clases

--- TurnAT/src/datos/Turno.java
+++ TurnAT/src/datos/Turno.java
@@ -1,5 +1,63 @@
 package datos;
 
-public class Turno {
+import java.time.LocalDate;
 
+import datos.Sucursal;
+import datos.Cliente;
+import datos.Empleado;
+import datos.Estado;
+import datos.Servicio;
+
+public class Turno {
+	private int idTurno;
+	private LocalDate fechaYHora;
+	private Servicio srv;
+	private Sucursal suc;
+	private Cliente cli;
+	private Empleado emp;
+	private Estado est;
+	public int getIdTurno() {
+		return idTurno;
+	}
+	protected void setIdTurno(int idTurno) {
+		this.idTurno = idTurno;
+	}
+	public LocalDate getFechaYHora() {
+		return fechaYHora;
+	}
+	public void setFechaYHora(LocalDate fechaYHora) {
+		this.fechaYHora = fechaYHora;
+	}
+	public Servicio getSrv() {
+		return srv;
+	}
+	public void setSrv(Servicio srv) {
+		this.srv = srv;
+	}
+	public Sucursal getSuc() {
+		return suc;
+	}
+	public void setSuc(Sucursal suc) {
+		this.suc = suc;
+	}
+	public Cliente getCli() {
+		return cli;
+	}
+	public void setCli(Cliente cli) {
+		this.cli = cli;
+	}
+	public Empleado getEmp() {
+		return emp;
+	}
+	public void setEmp(Empleado emp) {
+		this.emp = emp;
+	}
+	public Estado getEst() {
+		return est;
+	}
+	public void setEst(Estado est) {
+		this.est = est;
+	}
+	
+		
 }
