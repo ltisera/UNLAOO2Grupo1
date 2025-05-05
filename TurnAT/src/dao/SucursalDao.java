1bf80933d3423ffe9503eb7b526c2b1305ebd5fe
ltisera <lucastisera@gmail.com>
2025-05-05 20:15:35 -0300

    Test de clase Sucursal

---
+++ TurnAT/src/dao/SucursalDao.java
@@ -0,0 +1,88 @@
+package dao;
+import datos.Sucursal;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.query.Query;
+
+public class SucursalDao {
+	
+		private static Session session;
+		private Transaction tx;
+		private void iniciaOperacion() throws HibernateException {
+			session = HibernateUtil.getSessionFactory().openSession();
+			tx = session.beginTransaction();
+		}
+		
+		private void manejaExcepcion(HibernateException he) throws HibernateException {
+			tx.rollback();
+			throw new HibernateException("ERROR en la capa de acceso a datos", he);
+		}
+		
+		public int agregar(Sucursal objeto) {
+			int id = 0;
+			try {
+				iniciaOperacion();
+				id = Integer.parseInt(session.save(objeto).toString());
+				tx.commit();
+			} catch (HibernateException he) {
+				manejaExcepcion(he);
+			} finally {
+				session.close();
+			}
+			return id;
+		}
+		
+		public void actualizar(Sucursal objeto) {
+			try {
+				iniciaOperacion();
+				session.update(objeto);
+				tx.commit();
+			} catch (HibernateException he) {
+				manejaExcepcion(he);
+			} finally {
+				session.close();
+			}
+		}
+		
+		public void eliminar(Sucursal objeto) {
+			try {
+				iniciaOperacion();
+				session.delete(objeto);
+				tx.commit();
+			} catch (HibernateException he) {
+				manejaExcepcion(he);
+			} finally {
+				session.close();
+			}
+		}
+		
+		public Sucursal traer(int idSucursal) {
+			Sucursal objeto = null;
+			try {
+				iniciaOperacion();
+				objeto = (Sucursal) session.get(Sucursal.class, idSucursal);
+			} finally {
+				session.close();
+			}
+			return objeto;
+		}
+		
+		
+		public List<Sucursal> traer() {
+			List<Sucursal> lista = new ArrayList<Sucursal>();
+			try {
+				iniciaOperacion();
+				Query<Sucursal> query = session.createQuery("from sucursal s", Sucursal.class);
+				lista = query.getResultList();
+			} finally {
+				session.close();
+			}
+			return lista;
+		}
+}
---
+++ TurnAT/src/test/tstSuc.java
@@ -0,0 +1,16 @@
+package test;
+import dao.SucursalDao;
+import datos.Sucursal;
+public class tstSuc {
+
+	
+	public static void main(String[] args) {
+		Sucursal suc = new Sucursal("Rocio", "Una", 23344);
+		SucursalDao sdao = new SucursalDao();
+		sdao.agregar(suc);
+		System.out.println("POR QUE NO ME AUTO COMPLETA ");
+		// TODO Auto-generated method stub
+
+	}
+
+}
