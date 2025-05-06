8bfb8170b39835c7e8806c2295c80ad9b553b1d9
CarusoDavid21 <Nicowalfmer@outlook.com>
2025-05-06 19:44:12 -0300

    Adios a Cliente y Empleado (Sucursal ABM y test)

--- TurnAT/src/test/testSucursal.java
+++ TurnAT/src/test/testSucursal.java
@@ -5,13 +5,12 @@ import negocio.SucursalABM;
 public class testSucursal {
     public static void main(String[] args) {
         SucursalABM abm = new SucursalABM();
-
-        // Agregar una sucursal
-        int id = abm.agregar("Sucursal Centro", "Av. Principal 123", 123456789);
-        System.out.println("Sucursal creada con ID: " + id);
-
-        // Listar todas
-        System.out.println("Todas las sucursales:");
-        abm.traerTodas().forEach(System.out::println);
+        
+        // Intenta agregar la misma sucursal dos veces
+        abm.agregar("Sucursal Centro", "Av. Principal 123", 123456789);
+        abm.agregar("Sucursal Centro", "Av. Principal 123", 111111111); // Este fallará
+        
+        // Listar
+        abm.traerTodos().forEach(System.out::println);
     }
 }
