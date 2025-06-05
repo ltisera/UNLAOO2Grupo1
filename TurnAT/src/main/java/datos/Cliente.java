5ad2ebcd1dfb51cde51da7d64dc967fa385070b7
CarusoDavid21 <Nicowalfmer@outlook.com>
2025-06-05 08:08:26 -0300

    datos Empleado y Cliente

--- TurnAT/src/main/java/datos/Cliente.java
+++ TurnAT/src/main/java/datos/Cliente.java
@@ -1,5 +1,25 @@
 package datos;
 
-public class Cliente {
+public class Cliente extends Persona {
+	private String telefono;
 
+	public Cliente() {}
+
+	public Cliente(String nombre, String apellido, int dni, String email, Direccion direccion, String telefono) {
+		super(nombre, apellido, dni, email, direccion);
+		this.telefono = telefono;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + ", Cliente [telefono=" + telefono + "]";
+	}
 }
