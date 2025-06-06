9d6ff1896019b1946e93882f9eaec1fddc5324b3
ltisera <lucastisera@gmail.com>
2025-06-06 16:34:56 -0300

    Hibernando Una entidad hahhaha

--- TurnAT/src/main/java/com/turnat/TurnAT/TestDataRunner.java
+++ TurnAT/src/main/java/com/turnat/TurnAT/TestDataRunner.java
@@ -1,18 +1,18 @@
-/*package com.turnat.TurnAT;
+package com.turnat.TurnAT;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import com.turnat.TurnAT.models.entities.Cliente;
+import com.turnat.TurnAT.models.entities.Estado;
 import com.turnat.TurnAT.models.entities.Direccion;
-import com.turnat.TurnAT.repositories.IPersonaRepository;
+import com.turnat.TurnAT.repositories.IEstadoRepository;
 @Component
 
 public class TestDataRunner implements CommandLineRunner {
 	
-	private final IPersonaRepository repoP;
+	private final IEstadoRepository repoE;
 	
-	public TestDataRunner(IPersonaRepository repoP) {
-        this.repoP = repoP;
+	public TestDataRunner(IEstadoRepository repoE) {
+        this.repoE = repoE;
     }
 	
     @Override
@@ -21,15 +21,15 @@ public class TestDataRunner implements CommandLineRunner {
 
         // Ejemplo: Instanciando objetos Cliente y Empleado
         
-        Direccion dir = new Direccion("glew", "Alguna" , 7888);
-        Cliente cliente = new Cliente("juan", "poll", 2334233, "arro@arroba.com", dir, "+54 11 1111 1111");
+        //Direccion dir = new Direccion("glew", "Alguna" , 7888);
+        Estado est = new Estado("juan1111 1111");
         
         
         // Mostrar datos por consola (o usar repositorios si ya los tenés)
-        System.out.println("Cliente: " + cliente.getNombre() + ", Email: " + cliente.getEmail());
+        System.out.println("Estado:" + est.getDescripcion());
         
-        repoP.save(cliente);
+        repoE.save(est);
         
         // Aquí podrías llamar a repositorios para guardar estos datos si los configuraste.
     }
-}*/
+}
