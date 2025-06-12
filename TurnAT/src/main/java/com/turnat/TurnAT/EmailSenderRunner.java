a3b13321a452432b43a60a8c379e88016cf2e789
SantiagoSerrano27 <santiagogonzaloserrano@gmail.com>
2025-06-12 19:44:23 -0300

    cambio de alginas vistas

--- TurnAT/src/main/java/com/turnat/TurnAT/EmailSenderRunner.java
+++ TurnAT/src/main/java/com/turnat/TurnAT/EmailSenderRunner.java
@@ -1,5 +1,6 @@
 package com.turnat.TurnAT;
 
+import java.time.LocalTime;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -7,14 +8,19 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import com.turnat.TurnAT.models.entities.Empleado;
 import com.turnat.TurnAT.models.entities.Cliente;
 import com.turnat.TurnAT.models.entities.Direccion;
+import com.turnat.TurnAT.models.entities.Disponible;
 import com.turnat.TurnAT.models.entities.Rol;
+import com.turnat.TurnAT.models.entities.Servicio;
 import com.turnat.TurnAT.repositories.IClienteRepository;
 import com.turnat.TurnAT.repositories.IRolRepository;
 import com.turnat.TurnAT.services.interfaces.IClienteService;
 import com.turnat.TurnAT.services.interfaces.IEmailService;
+import com.turnat.TurnAT.services.interfaces.IEmpleadoService;
 import com.turnat.TurnAT.services.interfaces.IRolService;
+import com.turnat.TurnAT.services.interfaces.IServicioService;
 
 @Component
 public class EmailSenderRunner implements CommandLineRunner {
@@ -24,12 +30,16 @@ public class EmailSenderRunner implements CommandLineRunner {
     private final IEmailService emailService;
     private final IRolRepository repoRol;
     private final IRolService rolService;
-    public EmailSenderRunner(IClienteService clienteService, PasswordEncoder passwordEncoder, IEmailService emailService,IRolRepository repoRol,IRolService rolService) {
+    private final IEmpleadoService EmpleadoService;
+    private final IServicioService servicioService;
+    public EmailSenderRunner( IServicioService servicioService, IEmpleadoService EmpleadoService, IClienteService clienteService, PasswordEncoder passwordEncoder, IEmailService emailService,IRolRepository repoRol,IRolService rolService) {
         this.repoRol = repoRol;
     	this.ClienteService = clienteService;
         this.passwordEncoder = passwordEncoder;
         this.emailService = emailService;
         this.rolService = rolService;
+        this.EmpleadoService = EmpleadoService;
+        this.servicioService = servicioService;
     }
 
     @Override
@@ -64,10 +74,37 @@ public class EmailSenderRunner implements CommandLineRunner {
 		Set<Rol> roles = new HashSet<>(); //se crea el set de rol
 		roles.add(rol); //le meto el rol creado o levantado
 		nuevoCliente.setRoles(roles);//se lo meto al cliente
+		
 		  
 		  ClienteService.actualizar(nuevoCliente);
 
        */
+        //Crear el admin con el constructor completo
+    	/* Disponible disp = new Disponible(LocalTime.of(10,00) , LocalTime.of(18,00),true, true, true,true, true, true, true);
+         Servicio srv = new Servicio("Administracion", "Maneja la base de datos", LocalTime.of(8,0),disp);
+         servicioService.agregar(srv);
+        Empleado nuevoEmpleado = new Empleado(
+                "Santiago",                        // nombre
+                passwordEncoder.encode("papa"), // password encriptada
+                "Serrano",                         // apellido
+                455646518,                          // DNI
+                "santiagogonzaloserrano@gmail.com",// email
+                 null ,                       // dirección
+                "1122334455",				//legajo
+                srv							//servicio
+               
+        );
+        // Guardar el cliente
+		  EmpleadoService.agregar(nuevoEmpleado);
+        Rol rol = repoRol.findByNombre("ADMIN").orElseThrow(() -> new RuntimeException("Rol no encontrado")); 
+		   
+		Set<Rol> roles = new HashSet<>(); //se crea el set de rol
+		roles.add(rol); //le meto el rol creado o levantado
+		nuevoEmpleado.setRoles(roles);//se lo meto al empleado
+		
+		  
+		 EmpleadoService.actualizar(nuevoEmpleado);
+*/
 
         // Enviar correo de bienvenida
         //String asunto = "¡Bienvenido a TurnAT!";
