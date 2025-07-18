d0d6bfc42806055cb0219c554e53c62a14b57164
SantiagoSerrano27 <santiagogonzaloserrano@gmail.com>
2025-07-18 12:41:43 -0300

    Envio de mail arreglado, vista solicitar turno tambien

--- TurnAT/src/main/java/com/turnat/TurnAT/configurations/security/EmailConfiguration.java
+++ TurnAT/src/main/java/com/turnat/TurnAT/configurations/security/EmailConfiguration.java
@@ -1,5 +1,6 @@
 package com.turnat.TurnAT.configurations.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,13 +11,19 @@ import java.util.Properties;
 @Configuration
 public class EmailConfiguration {
 
-    @Bean
+	@Value("${spring.mail.username}")
+	private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+	
+	@Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername(System.getenv("MAIL_USERNAME"));
-        mailSender.setPassword(System.getenv("MAIL_PASSWORD"));
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.smtp.auth", "true");
--- TurnAT/src/main/java/com/turnat/TurnAT/services/implementations/EmailServiceImp.java
+++ TurnAT/src/main/java/com/turnat/TurnAT/services/implementations/EmailServiceImp.java
@@ -1,6 +1,7 @@
 package com.turnat.TurnAT.services.implementations;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,8 @@ public class EmailServiceImp implements IEmailService {
 
     @Autowired
     private JavaMailSender mailSender;
+    @Value("${spring.mail.username}")
+    private String remitente;
 
     @Override
     public void enviarCorreo(String para, String asunto, String cuerpo) {
@@ -19,7 +22,7 @@ public class EmailServiceImp implements IEmailService {
         mensaje.setTo(para);
         mensaje.setSubject(asunto);
         mensaje.setText(cuerpo);
-        mensaje.setFrom("oo2grupo01@gmail.com"); 
+        mensaje.setFrom(remitente); 
         
         try {
             mailSender.send(mensaje);
